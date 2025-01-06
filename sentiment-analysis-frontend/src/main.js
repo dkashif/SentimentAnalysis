e37b563734d71fa96fa9dee2e506c318f6b9cc02
dkashif <dkashif@arizona.edu>
2025-01-05 19:35:05 -0700

    gets rid of stuff from main.js for now

--- sentiment-analysis-frontend/src/main.js
+++ sentiment-analysis-frontend/src/main.js
@@ -3,16 +3,4 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.getElementById('reviewForm').addEventListener('submit', async function(event) {
-  event.preventDefault();
-  const review = document.getElementById('review').value;
-  const response = await fetch('http://localhost:5000:5173', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ review: review })
-  });
-  const data = await response.json();
-  document.getElementById('result').innerText = `Sentiment: ${data.sentiment}, Probability: ${data.probability}`;
-});
+
